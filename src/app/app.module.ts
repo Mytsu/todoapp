71f7b07f7483dcb9184328656c5947d160163f76
Mytsu <tlc.jooker@gmail.com>
2021-02-12 14:56:29 -0300

    setting up components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AppRoutingModule } from './app.routing.module';
 import { AppComponent } from './app.component';
+import { PageListComponent } from './containers/page-list.component';
+import { TodoComponent } from './components/todo/todo.component';
+import { LoginComponent } from './components/login/login.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageListComponent,
+    TodoComponent,
+    LoginComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    AppRoutingModule,
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/containers/page-list.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pagelist',
+  template: `
+
+  `,
+  styles: [`
+
+  `]
+})
+export class PageListComponent {
+
+}
