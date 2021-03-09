5254bd744a5835de87d21ceca33037fe33ace82e
Mytsu <tlc.jooker@gmail.com>
2021-03-09 15:51:10 -0300

    fix AuthService isLoggedIn, fix NewComponent width

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PageListComponent } from './containers/page-list.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
-import { TodoComponent } from './components/todo/todo.component';
 
 const routes: Routes = [
   { path: '', component: PageListComponent },
