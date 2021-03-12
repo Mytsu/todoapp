3bc4ec14eca0ff7380de648f39048ff357e055b2
Mytsu <tlc.jooker@gmail.com>
2021-03-12 14:23:42 -0300

    refactoring todoReducer to fit eslint rules

--- src/app/store/reducers/todo.reducer.ts
+++ src/app/store/reducers/todo.reducer.ts
@@ -1,5 +1,5 @@
 import { Action, createReducer, on } from '@ngrx/store';
-import { uuidv4 } from 'src/app/utils/uuidv4';
+import { uuidv4 } from './../../utils/uuidv4';
 import { Todo } from './../../models/todo.model';
 import { add, update, remove } from './../actions/todo.actions';
 
@@ -11,20 +11,18 @@ export const initialState: Todo[] = [
 
 const todoReducer = createReducer(
   initialState,
-  on(add, (state: Todo[], props: { todo: Todo }) => (
-    [...state, props.todo]
-  )),
+  on(add, (state: Todo[], props: { todo: Todo }) => [...state, props.todo]),
   on(update, (state: Todo[], props: { todo: Todo }) => {
     const newState: Todo[] = [];
-    state.map((todo, i) => {
+    state.map((todo, i) =>
       (todo.id === props.todo.id) ?
-        newState.push({ ...props.todo }) : newState.push(state[i]);
-    });
+        newState.push({ ...props.todo }) : newState.push(state[i])
+    );
     return newState;
   }),
-  on(remove, (state: Todo[], props: { id: string }) => {
-    return state.filter(todo => todo.id !== props.id);
-  })
+  on(remove, (state: Todo[], props: { id: string }) =>
+    state.filter((todo) => todo.id !== props.id)
+  )
 );
 
 export const reducer = (state: Todo[] | undefined, action: Action) =>
