f8384f59c2e7b8a11abcab76044a761266fef150
Mytsu <tlc.jooker@gmail.com>
2021-07-04 00:51:19 -0300

    setting up home component for development to-dos

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Todo } from 'src/app/models/todo.model';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  todos: Todo[] = [
+    { content: 'setup email/password login', done: false },
+    { content: 'profile popup menu', done: false },
+    { content: 'firebase security rules', done: false },
+    { content: 'serverless functions to retrieve app stats', done: false },
+    { content: 'routing animations', done: false },
+  ];
 
   constructor() { }
 
